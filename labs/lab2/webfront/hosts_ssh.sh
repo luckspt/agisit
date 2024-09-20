491727091a24912698ece4d4dfb447bd6a6d63df
Mafalda Sofia Carvalho Ferreira <mafaldasofiacf@tecnico.ulisboa.pt>
2024-09-20 08:21:03 +0000

    Minor update to lab2 hosts ssh script

--- labs/lab2/webfront/hosts_ssh.sh
+++ labs/lab2/webfront/hosts_ssh.sh
@@ -8,4 +8,6 @@
 # (PasswordAuthentication no)
 
 sudo sed -i 's/^\s*#\?PasswordAuthentication\s*\(yes\|no\)\s*$/PasswordAuthentication yes/' /etc/ssh/sshd_config
+# Apply to all .conf files that may be included in /etc/ssh/sshd_config -- suppress any error if no file is found
+sudo sed -i 's/^\s*#\?PasswordAuthentication\s*\(yes\|no\)\s*$/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/*.conf 2>/dev/null
 sudo systemctl restart sshd
